1c39376706968b96043b18332ed6024461e09712
brancoleonardo1 <161650075+brancoleonardo1@users.noreply.github.com>
2024-09-20 19:16:21 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // Cria uma árvore binária
+        
         ArvoreBinaria arvore = new ArvoreBinaria();
 
-        // Insere elementos na árvore
+    
         System.out.println("Inserindo valores na árvore:");
         int[] valoresParaInserir = {50, 30, 20, 40, 70, 60, 80};
         for (int valor : valoresParaInserir) {
@@ -11,11 +11,9 @@ public class Main {
             arvore.inserir(valor);
         }
 
-        // Mostra a árvore em ordem
         System.out.println("\nÁrvore em ordem após inserção:");
         arvore.emOrdem();
 
-        // Removendo alguns nós
         System.out.println("\n\nRemovendo nós da árvore:");
         int[] valoresParaRemover = {20, 30, 50};
         for (int valor : valoresParaRemover) {
